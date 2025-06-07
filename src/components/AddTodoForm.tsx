241d847b109b438e16f11ae1eef77b71139f8e20
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-07 10:17:04 +0800

    added AddTodoForm

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+
+interface AddTodoFormProps {
+	onSubmit: (title: string) => void
+}
+
+function AddTodoForm({ onSubmit }: AddTodoFormProps) {
+	const [input, setInput] = useState('')
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (!input.trim()) {
+			return
+		}
+		onSubmit(input)
+		setInput('')
+	}
+
+	return (
+		<form className="flex" onSubmit={handleSubmit}>
+			<input
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+				type="text"
+				placeholder="What needs to be done?"
+				className="rounded-s-md grow border border-gray-400 p-2"
+			/>
+			<button
+				type="submit"
+				className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+			>
+				Add
+			</button>
+
+		</form>
+	)
+}
+
+export default AddTodoForm
