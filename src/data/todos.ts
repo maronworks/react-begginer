54f7941d07ff1660a03587261f4aa17fa9578a34
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-06-07 09:35:09 +0800

    integrated tailwindcss, created type Todo and list of todo

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/Todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: 'Ask crushie to go out!',
+        completed: false,
+    },
+    {
+        id: 2,
+        title: 'Go out with crushie.',
+        completed: false,
+    },
+    {
+        id: 3,
+        title: 'Take a lot of pictures.',
+        completed: false
+    }
+]
